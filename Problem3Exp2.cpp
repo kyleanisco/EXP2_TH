cb2aac88df035f59bd0571901bb752958ea864ea
kyleanisco <47727580+kyleanisco@users.noreply.github.com>
2019-05-04 20:46:42 +0800

    Update Problem3Exp2.cpp

--- Problem3Exp2.cpp
+++ Problem3Exp2.cpp
@@ -7,47 +7,59 @@
 using namespace std;
 int main()
 {
-double x,y,v,num;
-int num1;
-const double z=2.5;
+  double x,y,v,num;
+  int num1;
+  const double z=2.5;
 
-cout <<"Input the value of x: ";
-cin>> x;
-cout << "Input the value of y: ";
-cin>>y;
+  cout <<"Input the value of x: ";
+  cin>> x;
+  cout << "Input the value of y: ";
+  cin>>y;
 
 switch(x<3)
 {
-case 1: 
-if(y>1&&y<5){
-v=x*y*z;
-cout << "V is equal to:"<<setw(10)<<v<<setprecision(2);
-}else if (y>=5){
-v=x+(y/z);
-cout << "V is equal to:"<<setw(10)<<v<<setprecision(2);}
-else{
-v=x+y+z;
-cout << "V is equal to:"<<setw(10)<<v<<setprecision(2);
+ case 1: 
+  if(y>1&&y<5)
+  {
+    v=x*y*z;
+    cout << "V is equal to:"<<setw(10)<<v<<setprecision(2);
+  }
+  else if (y>=5)
+  {
+    v=x+(y/z);
+    cout << "V is equal to:"<<setw(10)<<v<<setprecision(2);
+  }
+  else
+  {
+    v=x+y+z;
+    cout << "V is equal to:"<<setw(10)<<v<<setprecision(2);
+  }
+  break;
+ case 2:
+  if(y<=5)
+  {
+    num=(x-y)/z;
+    v=abs(num1);
+    cout << "V is equal to:"<<setw(10)<<v<<setprecision(2);
+  }
+  else if (y>5)
+  {
+    num=sqrt(x+z);
+    v=x-num;
+    cout << "V is equal to:"<<setw(10)<<v<<setprecision(2);
+  }
+  else
+  {
+    v=x+y+z;
+    cout << "V is equal to:"<<setw(10)<<v<<setprecision(2);
+  }
+  break;
+
+ default:
+  v=x+y+z;
+  cout << "V is equal to:"<<setw(10)<<v<<setprecision(2);
 }
-break;
-case 2:
-if(y<=5){
-num=(x-y)/z;
-v=abs(num1);
-cout << "V is equal to:"<<setw(10)<<v<<setprecision(2);
-}else if (y>5){
-num=sqrt(x+z);
-v=x-num;
-cout << "V is equal to:"<<setw(10)<<v<<setprecision(2);}
-else{
-v=x+y+z;
-cout << "V is equal to:"<<setw(10)<<v<<setprecision(2);
-}
-break;
-default:
-v=x+y+z;
-cout << "V is equal to:"<<setw(10)<<v<<setprecision(2);
-}
+  
 getch();
 return 0;
 }
