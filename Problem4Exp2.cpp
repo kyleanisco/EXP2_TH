6c509b42399c5a4b50ea30b3c0779be4dd98c35f
kyleanisco <47727580+kyleanisco@users.noreply.github.com>
2019-05-05 20:11:17 +0800

    Update Problem4Exp2.cpp

--- Problem4Exp2.cpp
+++ Problem4Exp2.cpp
@@ -11,14 +11,22 @@ using namespace std;
   		
   		for (i=1; i<=10; i++)
   		{
-  			cout << counter << " , ";
+  			cout << i << " , ";
   			counter ++;
 		}
 		
-		for (counter=12; counter <=30;)
+		for (counter=12; counter <=30; )
   		{
-  			cout << counter << " , ";
-  			counter +=2;  			
+  			if (counter==30)
+  			{
+			cout << counter << " ";
+			break;
+		}
+		else
+		{
+			cout << counter << " , ";
+  			counter +=2;
+		}
 		}
   		
 		getch ();
